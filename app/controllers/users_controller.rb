660e883aef5014024826c65cdb5c3016ebbebeb8
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-23 00:45:19 -0500

    create views for login and signup plus their controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb: "/users/signup"
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    redirect "/users/#{@user.slug}"
+  end
+
+  get '/login' do
+    erb: "/users/login"
+  end
+
 end

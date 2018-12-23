f10539f0fc387ab68a637040f20e4689b0f7cbf8
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-23 13:09:05 -0500

    add more functionality to user controller and create user home view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,32 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.create(params)
-    redirect "/users/#{@user.slug}"
+    if !!User.find_by(username: params[:username], email: params[:username])
+      redirect "/login"
+    else
+      @user = User.create(params)
+      redirect "/users/#{@user.slug}"
+    end
   end
 
   get '/login' do
     erb :"/users/login"
+  end
+
+  post '/login' do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/goals"
+    else
+      redirect "/signup"
+    end
+
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :"/users/home"
   end
 
 end

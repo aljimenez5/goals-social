548b450690986ad56a46df31f764a106442b854e
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-26 15:21:19 -0500

    routing content on page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,14 +3,15 @@ class GoalsController < ApplicationController
   get '/goals' do
     if logged_in?(session)
       @goals = Goal.all
-      erb :"/goals/home"
+      erb :"/users/home"
     else
       redirect "/login"
     end
   end
 
-  get '/goals/:id' do 
+  get '/goals/:id' do
     @goal = Goal.find(params[:id])
     erb :"/goals/show"
   end
+
 end
---
+++ more_functionality_ideas.md
@@ -0,0 +1,4 @@
+- Add a 'favorite' button to goals
+- Allow users to 'copy' a goal and/or step (track the copies for the original)
+- Add 'goals' categories
+- Add search bar for categories (or dropdown)

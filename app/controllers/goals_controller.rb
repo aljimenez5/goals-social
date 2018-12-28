c9c49ef0012232d8a80a96561ba15944ca41cb7a
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-28 03:22:25 -0500

    update views for each route and create edit function

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -10,11 +10,12 @@ class GoalsController < ApplicationController
   end
 
   post '/goals/new' do
-    goal = Goal.create(params[:goal])
-    current_user.goals << goal
+    @goal = Goal.create(params[:goal])
+    current_user.goals << @goal
     params[:steps].each do |step|
-      goal.steps << Step.create(params[:steps][step[0].to_sym])
+      @goal.steps << Step.create(params[:steps][step[0].to_sym]) unless params[:steps][step[0].to_sym][:content].empty?
     end
+    redirect "/users/#{current_user.slug}"
   end
 
   get '/goals/:id' do
@@ -22,6 +23,30 @@ class GoalsController < ApplicationController
     erb :"/goals/show"
   end
 
+  get '/goals/:id/edit' do
+    @goal = Goal.find(params[:id])
+    if current_user.id == @goal.user_id
+      erb :"/goals/edit"
+    else
+      redirect "/goals/#{@goal.id}"
+    end
+  end
+
+  patch '/goals/:id/edit' do
+    @goal = Goal.find(params[:id])
+    if current_user.id == @goal.user_id
+      @goal.update(params[:goal])
+      params[:steps].each do |step|
+        @step_obj = Step.find(step[0].to_i)
+        if !params[:steps][step[0].to_sym][:content].empty?
+          @step_obj.update(params[:steps][step[0].to_sym]) || @goal.steps << Step.create(params[:steps][step[0].to_sym])
+        elsif params[:steps][step[0].to_sym][:content].empty?
+          @step_obj.destroy
+        end
+      end
+      redirect "/goals/#{@goal.id}"
+    end
+  end
 
 
 end

3111028683946fa339962af6759876aac9d79140
aljimenez5 <arvida.jimenez@gmail.com>
2019-09-01 21:50:49 -0400

    restore files removed

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,13 @@
 require 'active_record'
+require 'bundler/setup'
+Bundler.require
+
+ENV['SINATRA_ENV'] ||= "development"
+
 
 
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/goals_social_db_dev')
+
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
